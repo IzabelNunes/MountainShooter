07ecdf472d7226e759350ee19cae2044a1fc77e6
Izabel <izabel_nunes2012@hotmail.com>
2024-12-16 23:25:47 -0300

    finalizado

--- code/Menu.py
+++ code/Menu.py
@@ -23,6 +23,7 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(15, "Izabel Nunes RU 1107603", C_ORANGE, ((WIN_WIDTH / 5), 30))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
